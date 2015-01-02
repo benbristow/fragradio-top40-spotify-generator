2ac96b0e89f175f4da052ef64a44cfdf03b7a71a
Ben Bristow <me@benbristow.co.uk>
2015-01-02 20:27:33 +0000

    First commit

---
+++ frag.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+require 'open-uri'
+require 'meta-spotify'
+require 'clipboard'
+
+#Don't edit below this line unless you know what you're doing.
+
+def getTopTracks
+  #Create new array
+  tracks = Array.new
+
+  #Extract top 40 tracks
+  @doc = Nokogiri::HTML(open("http://fragradio.com/includes/adjreq/web/playlist.php"))
+  @doc.xpath("//*[@id=\"top_requests\"]/dl/dd").each do |node|
+    tracks.push(node.xpath("a")[0]['title'])
+  end
+
+  #Return array
+  return tracks
+end
+
+def getSpotifyUris(tracks)
+  count = 0
+  outputString = ""
+  tracks.each do |track|
+    count += 1
+    begin
+      results = MetaSpotify::Track.search(track)
+      outputString += "\n #{results[:tracks][0].uri}"
+      puts "#{count}> Found Spotify URI for #{track}"
+    rescue NoMethodError
+      puts "#{count}> Can't find Spotify track for #{track}"
+    end
+  end
+
+  return outputString
+end
+
+puts "Spotify Playlist copied to clipboard. Just open Spotify on your desktop and ctrl+v into a playlist!"
+Clipboard.copy(getSpotifyUris(getTopTracks()))
